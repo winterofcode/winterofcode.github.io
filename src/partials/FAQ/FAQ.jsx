cee3cb590be847f227eb6837cc09b631003d4648
MYSELF-SAYAN <116438215+MYSELF-SAYAN@users.noreply.github.com>
2023-11-08 22:20:22 +0530

    message

--- src/partials/FAQ/FAQ.jsx
+++ src/partials/FAQ/FAQ.jsx
@@ -1,8 +1,75 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "./FAQ.scss";
-
+import faq from "./faq";
+import { CaretRightOutlined } from '@ant-design/icons';
+import { Collapse, Space } from "antd";
+import { theme, ConfigProvider } from "antd";
+console.log(faq);
 const FAQ = () => {
-  return <div>FAQ</div>;
+  const { token } = theme.useToken();
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  useEffect(() => {
+    
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640); 
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  return (
+    <div className="faq">
+      <h1 className="head-faq">FAQs</h1>
+      <ConfigProvider
+        theme={{
+          token: {
+            Collapse: {
+              headerPadding: "5px 10px",
+              contentPadding: "2px 10px ",
+              fontSize: isSmallScreen ? "10px" : "15px" // fixed syntax error
+            },
+            Input: {
+              colorPrimary: "#eb2f96",
+              algorithm: true, // Enable algorithm
+            },
+          },
+        }}
+      >
+        {faq.map((item, index) => {
+          return (
+            <Collapse
+            expandIcon={({ isActive }) => <CaretRightOutlined style={{
+              padding: "7px 2px",
+            }} rotate={isActive ? 90 : 0} />}
+              key={index}
+              accordion={true}
+              style={{
+                background: "#E0F4FF",
+                marginBottom: "20px",
+              }}
+            >
+              <Collapse.Panel
+                header={<div>{item.label}</div>}
+                key={item.key}
+               
+              >
+                <p
+                  className="accordion-text"
+                  style={{
+                    padding: "0px",
+                  }}
+                >
+                  {item.children}
+                </p>
+              </Collapse.Panel>
+            </Collapse>
+          );
+        })}
+      </ConfigProvider>
+    </div>
+  );
 };
 
 export default FAQ;
