b4a8dce4640345f4cd94639e61ff3936ce11dfcd
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2023-11-08 23:19:09 +0530

    working on Apply

--- src/partials/Apply/Apply.jsx
+++ src/partials/Apply/Apply.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Apply.scss";
 
 const Apply = () => {
-  return <div>Apply</div>;
+  return (
+    <>
+      <div className="box">
+        <div className="heading">
+          Apply
+        </div>
+        <div className="discription">
+          Start your journey with Winter Of Code & be a part of our Open Source community
+          <br />
+          Check the Timeline section .Applications will open on the mentioned date.
+        </div>
+      </div>
+    </>
+    )
 };
 
 export default Apply;
