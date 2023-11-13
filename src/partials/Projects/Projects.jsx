264024e35cb2218228f2e51d3cc089e2ce0d36c8
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2023-11-13 19:01:16 +0530

    Bugs Fixed

--- src/partials/Projects/Projects.jsx
+++ src/partials/Projects/Projects.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Projects.scss";
 import Cards from "components/Cards/Cards";
+import projectData from "constants/projectData";
 // import Card from 
 const Projects = () => {
   return (
     <>
         <div className="container">
-          
+        {projectData.map((projectData, index) =>
+            <div key={projectData.name + index}>
+              <Cards/>
+            </div>
+            )}
         </div>
     </>
   )
