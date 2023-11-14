ab73ce5330af039f75c084372ec1197755a71966
debojitsaha <saha05debojit@gmail.com>
2023-11-14 15:13:20 +0530

    Fixed: styling & html structure

--- src/partials/Apply/Apply.jsx
+++ src/partials/Apply/Apply.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Apply.scss";
-import { Button, Flex } from 'antd';
+import { Button, Flex } from "antd";
 
 // const boxStyle = {
 //   width: '100%',
@@ -12,29 +12,37 @@ const Apply = () => {
   return <div id="organization">Apply</div>;
 =======
   return (
-    
     <>
       <div className="box">
-        <div className="heading">
-          Apply
-        </div>
+        <div className="heading">Apply</div>
         <div className="discription">
-          Start your journey with Winter Of Code & be a part of our Open Source community
+          Start your journey with Winter Of Code & be a part of our Open Source
+          community
           <br />
-          Check the Timeline section .Applications will open on the mentioned date.
+          Check the Timeline section .Applications will open on the mentioned
+          date.
         </div>
         <div className="buttons">
-        {/* <Flex gap="12px" style={boxStyle} wrap="wrap"> */}
-          <Button type="primary" className="primary">Apply As Organisation</Button>
-          <Button type="primary" className="primary">Apply As Community Partner</Button>
-          <Button type="primary" className="primary">Apply As Mentor</Button>
-          <Button type="primary" className="primary">Apply As Student</Button>
-        {/* </Flex> */}
+          <div className="btn_wrap1">
+            <Button type="primary" className="primary">
+              Apply As Organisation
+            </Button>
+            <Button type="primary" className="primary">
+              Apply As Community Partner
+            </Button>
+          </div>
+          <div className="btn_wrap2">
+            <Button type="primary" className="primary">
+              Apply As Mentor
+            </Button>
+            <Button type="primary" className="primary">
+              Apply As Student
+            </Button>
+          </div>
         </div>
       </div>
     </>
     )
->>>>>>> b4a8dce ( working on Apply)
 };
 
 export default Apply;
