c448c30f0d0b59386ec103eaa95a7fe8d732425c
soumi guria <soumiguria.cse2022@nsec.ac.in>
2023-11-14 11:24:03 +0530

    modifications done

---
+++ src/assets/timelineIcons.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+export const StarFull = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={31}
+    height={31}
+    fill={undefined}
+    viewBox="0 0 25 24"
+  >
+    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z" />
+  </svg>
+);
--- src/partials/SOM/SOM.jsx
+++ src/partials/SOM/SOM.jsx
@@ -1,35 +1,53 @@
 import React from "react";
-import "./SOM.scss"; // Import the SCSS file
+import "./SOM.scss";
+// @ts-ignore
+import students from "assets/image 4.svg";
+// @ts-ignore
+import organisations from "assets/image 5.svg";
+// @ts-ignore
+import mentors from "assets/image 6.svg";
 
-
-const Build = () => {
+const SOM = () => {
   return (
     
       <div className="content-container">
-
-        <div className="card-container">
-          <div className="card">
+        <div className="title-container">
+          <h1 className="title">Let's Build Something Awesome Together</h1>
+          <br />
+          <p className="description">
+            Developer Students Club of NSEC brings to you Winter of Code which
+            is a month-long extravaganza of Open Source contribution, bringing
+            together students, organisations and mentors to build amazing
+            projects together. Here is what the program has to offer for
+            students, organisations, and mentors.
+          </p>
+          <br/>
+        </div>
+        <br/>
+        <div className="card-containerSOM">
+          <div className="cardSOM">
             <div className="card-header">
               <h2>Students</h2>
+              <br/>
             </div>
-            <img src="assets/motive_students.png" alt="Students" className="card-image" />
+            <img src={students} alt="Students" className="card-image" />
             <p className="card-description">
               Students who are interested can register for the Winter of Code by
               filling a form. Students who are selected will work with mentors
               of the organisation that they are accepted for. If accepted,
-              students amalgamate with their organisations prior to the start of
-              coding and learn more about their organisation’s community.
-              Students then have one month to code and meet the deadlines agreed
-              upon with their mentors. On successful completion of the program
-              students will be rewarded with some cool schwags.
+              students amalgamate with their organisations prior to the start
+              of coding and learn more about their organisation’s community.
+              Students then have one month to code and meet the deadlines
+              agreed upon with their mentors. On successful completion of the
+              program students will be rewarded with some cool schwags.
             </p>
           </div>
 
-          <div className="card">
+          <div className="cardSOM">
             <div className="card-header">
               <h2>Organisations</h2>
+              <br/>
             </div>
-              
             <p className="card-description">
               Open source organisations can apply to be mentor organisations.
               Once accepted, organisations discuss possible ideas with students
@@ -40,30 +58,31 @@ const Build = () => {
               interaction is a vital part of the program.
             </p>
             <img
-                src="assets/motive_organisations.png"
-                alt="Organisations"
-                className="card-image"
-              />
+              src={organisations}
+              alt="Organisations"
+              className="card-image"
+            />
           </div>
 
-          <div className="card">
+          <div className="cardSOM">
             <div className="card-header">
               <h2>Mentors</h2>
+              <br/>
             </div>
-            <img src="assets/motive_mentor.png" alt="Mentors" className="card-image" />
+            <img src={mentors} alt="Mentors" className="card-image" />
             <p className="card-description">
               Mentors guide the students throughout their projects and review
               their code samples multiple times. They provide valuable feedback
-              and suggest possible improvements to the code. They also determine
-              if a student has successfully completed the Winter of code 2020
-              project. Students are then notified of the pass/fail status and
-              schwags are delivered accordingly.
+              and suggest possible improvements to the code. They also
+              determine if a student has successfully completed the Winter of
+              code 2020 project. Students are then notified of the pass/fail
+              status and schwags are delivered accordingly.
             </p>
           </div>
         </div>
       </div>
-
+    
   );
 };
 
-export default Build;
+export default SOM;
