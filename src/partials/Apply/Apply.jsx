0f38c837fd675645bb02b48c54ca8bf3e1179a71
debojitsaha <saha05debojit@gmail.com>
2023-11-14 15:49:09 +0530

    rebase fixing

--- src/partials/Apply/Apply.jsx
+++ src/partials/Apply/Apply.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./Apply.scss";
 
 const Apply = () => {
+<<<<<<< HEAD
   return <div id="organization">Apply</div>;
+=======
+  return (
+    <>
+      <div className="box">
+        <div className="heading">
+          Apply
+        </div>
+        <div className="discription">
+          Start your journey with Winter Of Code & be a part of our Open Source community
+          <br />
+          Check the Timeline section .Applications will open on the mentioned date.
+        </div>
+      </div>
+    </>
+    )
+>>>>>>> b4a8dce ( working on Apply)
 };
 
 export default Apply;
