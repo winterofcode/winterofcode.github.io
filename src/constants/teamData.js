0c41c6d2838bda947bd4cc8f48ab85b2fe914d69
debojitsaha <saha05debojit@gmail.com>
2023-11-16 13:26:36 +0530

    Updated: Team Data structure

--- src/constants/teamData.js
+++ src/constants/teamData.js
@@ -1,122 +1,50 @@
+// @ts-nocheck
+import debojit from "assets/TeamData/Debojit.jpg";
+
 export const teamData = [
   {
     name: "Debojit Saha",
     title: "Web Lead",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    image: debojit,
     socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
+      {
+        url: "https://github.com/debojitsaha/",
+        title: "GitHub",
+        icon: "https://img.icons8.com/3d-fluency/32/github.png",
+      },
+      {
+        url: "https://www.linkedin.com/in/debojit-saha/",
+        title: "LinkedIn",
+        icon: "https://img.icons8.com/color/32/linkedin.png",
+      },
+      {
+        url: "https://twitter.com/star_debojit",
+        title: "Twitter",
+        icon: "https://img.icons8.com/nolan/32/twitterx.png",
+      },
     ],
   },
   {
     name: "Bipasha Roy",
     title: "Web Team",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    image:
+      "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
     socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Debojit Saha",
-    title: "Web Lead",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Bipasha Roy",
-    title: "Web Team",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Debojit Saha",
-    title: "Web Lead",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Bipasha Roy",
-    title: "Web Team",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Debojit Saha",
-    title: "Web Lead",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Bipasha Roy",
-    title: "Web Team",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Debojit Saha",
-    title: "Web Lead",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Bipasha Roy",
-    title: "Web Team",
-    image: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Debojit Saha",
-    title: "Web Lead",
-    image: "https://via.placeholder.com/150",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
-    ],
-  },
-  {
-    name: "Bipasha Roy",
-    title: "Web Team",
-    image: "https://via.placeholder.com/150",
-    socials: [
-      { url: 'https://github.com/johndoe', title: 'GitHub', icon: "https://img.icons8.com/3d-fluency/32/github.png" },
-      { url: 'https://www.linkedin.com/in/johndoe', title: 'LinkedIn',icon: "https://img.icons8.com/color/32/linkedin.png"},
-      {url: 'https://www.linkedin.com/in/johndoe', title: 'Twitter',icon:"https://img.icons8.com/nolan/32/twitterx.png"}
+      {
+        url: "https://github.com/johndoe",
+        title: "GitHub",
+        icon: "https://img.icons8.com/3d-fluency/32/github.png",
+      },
+      {
+        url: "https://www.linkedin.com/in/johndoe",
+        title: "LinkedIn",
+        icon: "https://img.icons8.com/color/32/linkedin.png",
+      },
+      {
+        url: "https://www.linkedin.com/in/johndoe",
+        title: "Twitter",
+        icon: "https://img.icons8.com/nolan/32/twitterx.png",
+      },
     ],
   },
 ];
