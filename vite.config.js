3eec9441c6c2ba284cf4724848839d99ff028a35
cosmo3769 <piyushthakur3769@gmail.com>
2023-11-18 20:50:56 +0530

    fix deploy 3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import jsconfigPaths from "vite-jsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://winterofcode.github.io/',
   build: {
     outDir: "build",
     sourcemap: false,
