5f97cad5aa737dcf832fb7fed6bc68c8175e68da
Atif Ali <atif276ali@gmail.com>
2024-01-19 15:56:29 +0530

    Added community partners section

---
+++ src/constants/communityData.js
@@ -0,0 +1,32 @@
+export const community = [
+  {
+    name: "GDSC Aliah",
+    url: "/src/assets/CommunityData/GDSC Aliah logo - Ankit Majee.png",
+    link: "https://www.linkedin.com/company/gdsc-aliah/",
+  },
+  {
+    name: "GDSC BBIT",
+    url: "/src/assets/CommunityData/Group 1 - GDSC BBIT.png",
+    link: "",
+  },
+  {
+    name: "ClueLess",
+    url: "/src/assets/CommunityData/clueless - Subho Ghosh.png",
+    link: "https://www.linkedin.com/company/79089507",
+  },
+  {
+    name: "GDSC IIIT Kalyani",
+    url: "/src/assets/CommunityData/GDSC Logo chapter lockup centered template - Google Developer Student Clubs IIIT Kalyani.jpg",
+    link: "https://www.linkedin.com/company/gdsc-iiit-kalyani/",
+  },
+  {
+    name: "ACM SSCBS",
+    url: "/src/assets/CommunityData/acmlogo - Vineet.jpeg",
+    link: "https://www.linkedin.com/in/acm-sscbs/",
+  },
+  {
+    name: "Devorld",
+    url: "/src/assets/CommunityData/20230207_171323_0000 - Harshavardhan Bajoria.png",
+    link: "https://www.linkedin.com/company/devorld/",
+  },
+];
