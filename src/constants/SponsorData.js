548e51bc8478d259aca92b3136c5c4256bfc11bb
debojitsaha <saha05debojit@gmail.com>
2024-01-21 22:10:08 +0530

    update: sponsors card

--- src/constants/SponsorData.js
+++ src/constants/SponsorData.js
@@ -1,34 +1,31 @@
-const Data = [
+const sponsorData = [
   {
     id: 1,
     name: "GitHub",
-    profileImage:
-      "../SponsorsAssets/github.png",
+    profileImage: "../SponsorsAssets/github.png",
     websiteLink: "https://www.github.com",
     linkedInLink: "https://www.linkedin.com/company/github",
-    tag: "GigaByte",
-    bgColor: "goldenrod"
+    tag: "Gigabyte",
+    bgColor: "#FACC15",
   },
   {
     id: 2,
     name: "Loft Labs",
-    profileImage:
-      "../SponsorsAssets/Loft.png",
+    profileImage: "../SponsorsAssets/Loft.png",
     websiteLink: "https://www.loft.sh",
     linkedInLink: "https://www.linkedin.com/company/loft-sh",
-    tag: "GigaByte",
-    bgColor: "goldenrod"
+    tag: "Gigabyte",
+    bgColor: "#FACC15",
   },
   {
     id: 3,
     name: "Presar",
-    profileImage:
-      "../SponsorsAssets/presar.jpg",
-    websiteLink: "https://www.janesmithdesigns.com",
+    profileImage: "../SponsorsAssets/presar.jpg",
+    websiteLink: "https://presear.com/",
     linkedInLink: "https://www.linkedin.com/in/janesmith",
     tag: "Byte",
-    bgColor: "transparent"
+    bgColor: "#C0C0C0",
   },
 ];
 
-export default Data;
+export default sponsorData;
--- src/partials/Sponsors/Sponsors.jsx
+++ src/partials/Sponsors/Sponsors.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Data from "../../constants/SponsorData";
+import sponsorData from "../../constants/sponsorData";
 import SponsorsCard from "./SponsorsCard";
 import "./Sponsors.scss";
 
@@ -7,8 +7,8 @@ function Sponsors() {
   return (
     <div style={{marginBottom: "30px"}}>
       <h1 style={{ textAlign: "center", marginBottom: "20px" }}>Sponsors</h1>
-      <div className="Sponsor-section">
-        {Data.map((user) => (
+      <div className="sponsor-section">
+        {sponsorData.map((user) => (
           <SponsorsCard key={user.id} user={user} />
         ))}
       </div>
